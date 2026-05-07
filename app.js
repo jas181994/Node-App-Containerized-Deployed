8fa0ee9caf9caf5913b65c5674eb9f8e1376d0dd
jas181994 <jasmeet181994@gmail.com>
2026-05-07 18:17:10 +0530

    new

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('node app deployed through Github actions tested on eks tested through argocd. ')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
